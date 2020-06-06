77dbd1d3834f715bb88666e6592feb1ee8c5a2ff
rafaelhenter <rafael@henter.com.br>
2020-06-06 16:01:07 -0300

    do not raise a exception on 400 (Bad Request)

--- supplai_client/validators.py
+++ supplai_client/validators.py
@@ -13,7 +13,7 @@ def validate_status_code(status_code, content):
     elif status_code == status.HTTP_404_NOT_FOUND:
         exception_class = NotFound(status_code,
                                    {'errorMessage': 'Endpoint Not Found'})
-    elif status_code in range(status.HTTP_400_BAD_REQUEST, status.HTTP_417_EXPECTATION_FAILED + 1):
+    elif status_code in range(status.HTTP_405_METHOD_NOT_ALLOWED, status.HTTP_417_EXPECTATION_FAILED + 1):
         exception_class = SupplaiError(status_code, content)
     elif status.is_server_error(status_code):
         exception_class = ServerError(status_code)
